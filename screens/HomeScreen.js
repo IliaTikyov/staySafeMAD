fa8ae4beb054f3a4ba641824db76879cb9a11be9
Muhammad Saad <muhammadsaad8697@gmail.com>
2025-02-26 13:31:45 +0000

    Implemented Add Trip Button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => navigation.navigate("Add")}
+      >
+        <Text style={styles.buttonText}>Add Trip ➕</Text>
+      </TouchableOpacity>
+
       <Text style={styles.text}>This is the face of StaySafe app</Text>
     </View>
   );
@@ -12,14 +19,35 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#f8f9fa",
+    paddingTop: 16,
+  },
+  addButton: {
+    backgroundColor: "#42a5f5",
+    padding: 16,
+    borderRadius: 8,
+    alignItems: "center",
+    alignSelf: "stretch",
+    marginHorizontal: 16,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 16,
+  },
+  content: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#f8f9fa",
   },
   text: {
     fontSize: 18,
     fontWeight: "bold",
     color: "#333",
+    paddingTop: 20,
+    textAlign: "center",
   },
 });
 
