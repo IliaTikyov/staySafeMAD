227917d020cf9cb12b8539fff195e47f47e442a2
Muhammad Saad <muhammadsaad8697@gmail.com>
2025-03-14 15:00:19 +0000

    added my home ip address for quick access

--- api/apiClient.js
+++ api/apiClient.js
@@ -1,7 +1,7 @@
 //!!!!!!!!!!!!!!!!!!!!!!!!!!Warning!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 //!!!!!!!!!!!!!!!!!!!!!!If http://localhost:5000/staysafe/v1/api is not working!!!!!!!!!!!!!!!!!!!!!!!
-//!!!!!!!!!!!!!!!!!!!!!Replace it with your ipv4 address!!!!!!!!!!!!!!!!!!!!!!!!
-const API_BASE_URL = "http://10.154.69.178:5000/staysafe/v1/api";
+//!!!!!!!!!!!!!!!!!!!!!Replace it with your ipv4 address!!!!!!!!!!!!!!!!!!!!!!!! muhammad's home ipv4 address '192.168.0.95' !!!
+const API_BASE_URL = "http://192.168.0.95:5000/staysafe/v1/api";
 
 export const apiRequest = async (endpoint, method = "GET", body = null) => {
   try {
